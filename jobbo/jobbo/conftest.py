03da417b0428e54ceac296cb0dc70fe58a8281d8
Kasia <katarzyna.jagiela@tylko.com>
2022-01-22 21:35:55 +0100

    Init stuff with cookiecutter

---
+++ jobbo/jobbo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from jobbo.users.models import User
+from jobbo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
