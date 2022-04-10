3800e159cd8382579bf7efe496f5f8e0ee842d9a
Kasia <katarzyna.jagiela@tylko.com>
2022-04-10 22:23:56 +0200

    Add users app

--- apps/conftest.py
+++ apps/conftest.py
@@ -1,14 +1,6 @@
 import pytest
 
-from jobbo.users.models import User
-from jobbo.users.tests.factories import UserFactory
-
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-@pytest.fixture
-def user() -> User:
-    return UserFactory()
