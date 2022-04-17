f85c251ae2fdd742fa158feb26dae010e8bc2aa0
Kasia <katarzyna.jagiela@tylko.com>
2022-04-18 00:18:39 +0200

    Add tests and typing to managers

--- apps/grabbo/managers.py
+++ apps/grabbo/managers.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class CompanyManager(models.Manager):
-    def get_possible_match(self, name):
+    def get_possible_match(self, name: str) -> models.QuerySet:
         """There are different company names on different boards."""
         # TODO: matching by company url
         stripped_name = name.replace('sp. z o.o.', '').strip()
-        return self.filter(name__iexact=stripped_name)
+        # if name didn't have sp. z o.o., let's add it to the check
+        name_with_sp = f'{stripped_name} sp. z o.o.'
+        return (
+            self.filter(name__iexact=stripped_name)
+            | self.filter(name__iexact=name_with_sp)
+        )
---
+++ apps/grabbo/tests/test_managers.py
@@ -0,0 +1,54 @@
+import pytest
+
+from apps.grabbo.managers import CompanyManager
+from apps.grabbo.models import Company
+
+
+@pytest.mark.django_db
+class TestCompanyManager:
+
+    def test_get_possible_match_returns_companies_with_same_name(
+        self,
+        company_factory,
+    ):
+        company_factory.create_batch(2, name='Fake Company')
+        manager = CompanyManager()
+        manager.model = Company
+        matches = manager.get_possible_match('Fake Company')
+        assert matches.count() == 2
+
+    @pytest.mark.parametrize(
+        'company1_name, company2_name',
+        [
+            ('Fake Company', 'Fake Company sp. z o.o.'),
+            ('Fake Company sp. z o.o.', 'Fake Company'),
+            ('Fake Company sp. z o.o.', 'Fake Company sp. z o.o.'),
+        ],
+    )
+    def test_get_possible_match_returns_companies_with_similar_name(
+        self,
+        company_factory,
+        company1_name,
+        company2_name,
+    ):
+        company_factory(name=company1_name)
+        company_factory(name=company2_name)
+        manager = CompanyManager()
+        manager.model = Company
+        matches = manager.get_possible_match(company1_name)
+        assert matches.count() == 2
+
+    @pytest.mark.parametrize(
+        'company_name',
+        ['Flake Company', 'Flakes Company sp. z o.o.', 'A Company'],
+    )
+    def test_get_possible_match_doesnt_return_companies_with_different_names(
+        self,
+        company_factory,
+        company_name,
+    ):
+        company_factory(name=company_name)
+        manager = CompanyManager()
+        manager.model = Company
+        matches = manager.get_possible_match('Fake Company')
+        assert matches.count() == 0
